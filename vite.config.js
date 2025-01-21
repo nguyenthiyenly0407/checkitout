32cc323de0e44c3ad43f70f80105869e03bed116
Nguyen Thi Yen Ly <nguyenlyyenthi@gmail.com>
2025-01-22 04:13:17 +0700

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: '/checkitout/', // Thay 'checkitout' bằng tên repository của bạn
-}
+export default defineConfig({
+  plugins: [react()],
+})
